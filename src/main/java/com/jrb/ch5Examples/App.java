ada4efa13bbec7cec859c138c0173fdb6a6ec0e7
Rob Bodenhamer <rob@robs-mbp.attlocal.net>
2018-04-25 19:14:42 -0500

    first commit

---
+++ src/main/java/com/jrb/ch5Examples/App.java
@@ -0,0 +1,13 @@
+package com.jrb.ch5Examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
